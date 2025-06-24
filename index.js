7d1b24a57e823582fc591e400bb4aba9c1ea17af
Neeraj Jangra <neeraj.jangra@grapecity.com>
2025-06-24 17:57:12 +0530

    connect with MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
 
 dotenv.config();
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((error) => {
+    console.log(`Error while connecting DB ${error}`);
+  });
+
 const app = express();
 const PORT = process.env.PORT;
 
